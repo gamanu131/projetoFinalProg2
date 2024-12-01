875713314777c27e4da356fa72ccffe2d06f71ae
vitinhomartins <vitinhocomputacao@gmail.com>
2024-11-30 23:13:20 -0300

    Add files via upload

--- trabalhoFinalProg2/code/main.cpp
+++ trabalhoFinalProg2/code/main.cpp
@@ -4,28 +4,44 @@
 #include <fstream>
 #include <locale.h>
 #include <string>
+
+
 using namespace std;
+
+const string users_direct = "users";
+
+typedef struct {
+    string mainName;
+    string fullName;
+    string sinopse; 
+    float rating;
+    int nRatings;
+    string comments[5];
+} content;
+
+typedef struct {
+    string nick;
+    string pass; 
+    int adm;
+} UserInfo;
+
+UserInfo usuario;
 
 bool logando(){
     setlocale(LC_ALL, "pt-br");
 
-    string username, userpass, user, pass;
-
-    /*cout << "Escreva seu nome de usuário: ";
-    cin.get();
-    getline(cin, username);
-    cout << "Escreva sua senha: ";
-    getline(cin, userpass);
-    cin.ignore();*/
-
+    string username, userpass, user, pass, adm;
     cout << "Escreva seu nome de usuário: "; cin >> username;
     cout << "Escreva sua senha: "; cin >> userpass;
 
     ifstream read(username + ".txt");
     getline(read, user);
     getline(read, pass);
-
+    getline(read, adm);
     if(user==username && pass==userpass){
+        usuario.nick = username; // coloca os valores das informações em suas respectivas lacunas nas structs. 
+        usuario.pass = userpass;
+        usuario.adm = stoi(adm); // transforma a string em um inteiro para validar adm. 
         return true;
     }else{
         return false;
@@ -36,48 +52,45 @@ void registro(){
     setlocale(LC_ALL, "pt-br");
 
     string username, userpass;
-
-    /*cout << "Escolha um nome de usuário: " << endl;
-    cin.get();
-    getline(cin, username);
-    cout << "Escolha uma senha: " << endl;
-    getline(cin, userpass);
-    cin.ignore();*/
-
+    int adm = 0; // primeira criação de conta, o usuário jamais será adm. (administração poderá ser setada por texto no banco de dados).
     cout << "Escolha um nome de usuário: " << endl; cin >> username;
     cout << "Escolha uma senha: " << endl; cin >> userpass;
 
     ofstream file;
     file.open(username+".txt");
-    file << username << endl << userpass;
-    file.close();
-    
+    file << username << endl << userpass << endl << adm;
+    file.close();   
 }
 
 int main(){
     setlocale(LC_ALL, "pt-br");
 
-    int escolha;
+    int escolha = 0;
      
     cout << "Bem vindo ao sistema de avaliação!" << endl << "Para login digite 0, para nova conta digite 1" << endl;
     cin >> escolha;
-    if(escolha==1){
-        registro();
-    }else if(escolha==0){
-        bool status = logando();
-        if (!status){
-            cout << "Informacao incorreta, tente novamente!" << endl;
-            system("PAUSE");
-            return 0;
-        }else{
-            cout << "Sucesso no login!" << endl;
-            system("PAUSE");
-            return 1;
-        }
-    }else{
+    while(escolha != 0 && escolha != 1) {
         cout << "Input invalido, tente novamente!" << endl;
+        cout << "Para login digite 0, para nova conta digite 1" << endl;
+        cin >> escolha;
     }
-
+    switch(escolha) {
+        case 1:
+            registro();
+            break;
+        case 0:
+            bool status = logando();
+            if (!status){
+                cout << "Informacao incorreta, tente novamente!" << endl;
+                system("PAUSE");
+                return 0;
+            }else{
+                cout << "Sucesso no login!" << endl;
+                system("PAUSE");
+                return 1;
+            }
+            break;
+    }
     return 0;
 
 }
